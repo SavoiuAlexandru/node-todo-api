188d632301fa320c58b1234189c8a9e868c3a361
alexandru.savoiu <alexandru.savoiu@resmed.com.au>
2018-12-19 17:48:51 +0200

    initial commit

---
+++ playground/mongo-find.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to database TodoApp');
+    }
+
+    console.log('connected to mongo db server');
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').find({_id: new ObjectID('5c1a4fa03e88a72978c8e780')}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    client.close();
+});
