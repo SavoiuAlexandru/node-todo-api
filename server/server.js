21262c7ca20d17b3718e08398e62d00c8920eedd
alexandru.savoiu <alexandru.savoiu@resmed.com.au>
2019-01-08 11:26:10 +0200

    added update and delete routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
@@ -46,11 +48,56 @@ app.get('/todos/:id', (req, res) => {
         }
 
         res.send({todo});
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     })
 });
 
+app.delete('/todos/:id', (req, res) => {
+    console.log(req.body);
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    console.log(req.body);
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(port, () => {
     console.log('Started on port' + port);
